ab0c0e1d49d7ae097ad4ac444dcbe5ebd416e7ce
Vladislav Morgunov <Zoncovsky@icloud.com>
2025-02-22 12:19:49 +0200

    added pagination and search (#15)
    * added pagination and search
    
    * added sorting, upgrade filter

--- app/controllers/main/dashboards_controller.rb
+++ app/controllers/main/dashboards_controller.rb
@@ -1,22 +1,41 @@
 module Main
   class DashboardsController < ApplicationController
+    before_action :load_categories, only: :catalog
+    before_action :load_products, only: :catalog
+
     def index
       @popular_products = Product.popular
-      @new_products = Product.new_arrivals
     end
 
     def catalog
-      @main_categories = Category.all
-
-      if params[:category_id].present?
-        @products = Product.where(category_id: params[:category_id])
-      else
-        @products = Product.all.order(created_at: :desc)
-      end
+      @pagy, @products = pagy(@search_products, limit: 6)
     end
 
     def contact; end
-
     def cart; end
+
+    private
+
+    def load_categories
+      @main_categories = Category.pluck(:id, :name)
+    end
+
+    def load_products
+      @search = Product.ransack(search_params[:q])
+      @search_products = Product.where(category_filter).merge(@search.result(distinct: true))
+      @search_products = @search_products.order(sorting_order)
+    end
+
+    def search_params
+      params.permit(:sort, q: {})
+    end
+
+    def category_filter
+      params[:category_id].present? ? { category_id: params[:category_id] } : {}
+    end
+
+    def sorting_order
+      search_params[:sort].presence || 'created_at DESC'
+    end
   end
 end
