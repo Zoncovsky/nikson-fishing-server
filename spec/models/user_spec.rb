5f9278d72b0c114b8fcf24eebe69ac9e1b14f82d
Vladislav Morgunov <87144955+Zoncovsky@users.noreply.github.com>
2025-02-27 14:40:58 +0200

    added authorize via google

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,26 +14,5 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
-    it { is_expected.to validate_presence_of(:phone_number) }
-  end
-
-  describe '#password_complexity' do
-    context 'when password is valid' do
-      let(:user) { build(:user, password: 'Password1') }
-
-      it 'does not add any errors' do
-        user.valid?
-        expect(user.errors[:password]).to be_empty
-      end
-    end
-
-    context 'when password is invalid' do
-      let(:user) { build(:user, password: 'password') }
-
-      it 'adds an error' do
-        user.valid?
-        expect(user.errors[:password]).to include('Пароль має містити хоча б одну велику літеру та не повинен містити пробілів')
-      end
-    end
   end
 end
