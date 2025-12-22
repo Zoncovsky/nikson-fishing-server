aeeba901842819741c94298945494805d2ef301a
vmorgunov <vmorgunov10@gmail.com>
2025-12-23 01:04:21 +0200

    test config

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -14,14 +14,17 @@
 # end
 
 ActiveRabbit::Client.configure do |config|
-  # config.api_key = '83db4c5f00a7b6449e38ceeef5da3670f43b6dfff864189fe40cfef275526beb'
+  # config.api_key = '72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3'
+
+  # local
+  # config.api_key = '12ad3efe0068fed92c5a1314daea05f19b9c85aa695ee189d04b1845af923ca7'
 
   # Enable monitoring features
   # config.enable_performance_monitoring = true
   # config.enable_n_plus_one_detection   = true
   # config.enable_pii_scrubbing          = true
 
-  config.api_url = 'http://65.108.93.69'
+  # config.api_url = 'http://65.108.93.69'
 
   # config.ignore_404 = true
 end
