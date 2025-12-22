86bd7d628ba50b6619767497d291171db15e951e
vmorgunov <vmorgunov10@gmail.com>
2025-12-23 01:30:08 +0200

    add config

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -1,16 +1,15 @@
-# ActiveRabbit::Client.configure do |config|
-  # config.api_key = '72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3'
-
-  # local
-  # config.api_key = '12ad3efe0068fed92c5a1314daea05f19b9c85aa695ee189d04b1845af923ca7'
+ActiveRabbit::Client.configure do |config|
+  config.api_key = "72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3"
 
   # Enable monitoring features
-  # config.enable_performance_monitoring = true
-  # config.enable_n_plus_one_detection   = true
-  # config.enable_pii_scrubbing          = true
-  # config.disable_console_logs = false
+  config.enable_performance_monitoring = true
+  config.enable_n_plus_one_detection   = true
+  config.enable_pii_scrubbing          = true
 
-  # config.api_url = 'http://65.108.93.69'
+  # Optional: enable deployment tracking
+  config.revision = ENV["APP_REVISION"] || `git rev-parse HEAD`.chomp
 
-  # config.ignore_404 = true
-# end
+  # Optional: 404 handling (true = ignore 404s, false = report them)
+  config.ignore_404 = true
+end
+
