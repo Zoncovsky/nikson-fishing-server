90d9b268da003221a116ec84ee5b504469b2cf7b
vmorgunov <vmorgunov10@gmail.com>
2025-12-23 01:23:22 +0200

    remove config

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -1,19 +1,4 @@
 # ActiveRabbit::Client.configure do |config|
-#   config.api_key = 'd1d1436596e96f782e19b5b305f6eeb0834e362d426f000dba83371e89ccb9bb'
-
-#   # Enable monitoring features
-#   config.enable_performance_monitoring = true
-#   config.enable_n_plus_one_detection = true
-#   config.enable_pii_scrubbing = true
-
-#   config.api_url = 'http://localhost:3000'
-#   # config.disable_console_logs = false
-
-#   config.ignore_404 = false
-#   # config.revision = ENV["APP_REVISION"] || `git rev-parse HEAD`.chomp
-# end
-
-ActiveRabbit::Client.configure do |config|
   # config.api_key = '72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3'
 
   # local
@@ -23,8 +8,9 @@ ActiveRabbit::Client.configure do |config|
   # config.enable_performance_monitoring = true
   # config.enable_n_plus_one_detection   = true
   # config.enable_pii_scrubbing          = true
+  # config.disable_console_logs = false
 
   # config.api_url = 'http://65.108.93.69'
 
   # config.ignore_404 = true
-end
+# end
