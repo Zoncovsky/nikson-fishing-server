a2ce108ea67cb68df7a6abca70e0205b1932a111
vmorgunov <vmorgunov10@gmail.com>
2025-12-23 01:51:24 +0200

    remove config

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -1,17 +1,17 @@
-ActiveRabbit::Client.configure do |config|
-  config.api_key = "72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3"
+# ActiveRabbit::Client.configure do |config|
+#   config.api_key = "72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3"
 
-  # Enable monitoring features
-  config.enable_performance_monitoring = true
-  config.enable_n_plus_one_detection   = true
-  config.enable_pii_scrubbing          = true
+#   # Enable monitoring features
+#   config.enable_performance_monitoring = true
+#   config.enable_n_plus_one_detection   = true
+#   config.enable_pii_scrubbing          = true
 
-  # Optional: enable deployment tracking
-  config.revision = ENV["APP_REVISION"] || `git rev-parse HEAD`.chomp
-  config.api_url = "https://api.activerabbit.ai"
-  config.disable_console_logs = false
+#   # Optional: enable deployment tracking
+#   config.revision = ENV["APP_REVISION"] || `git rev-parse HEAD`.chomp
+#   config.api_url = "https://api.activerabbit.ai"
+#   config.disable_console_logs = false
 
-  # Optional: 404 handling (true = ignore 404s, false = report them)
-  config.ignore_404 = true
-end
+#   # Optional: 404 handling (true = ignore 404s, false = report them)
+#   config.ignore_404 = true
+# end
 
