0996d452c96ba92a1cf61f566d4e682276b45c2e
vmorgunov <vmorgunov10@gmail.com>
2025-12-23 02:19:42 +0200

    update ar config

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -1,5 +1,5 @@
-ActiveRabbit::Client.configure do |config|
-#   config.api_key = "72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3"
+# ActiveRabbit::Client.configure do |config|
+   # config.api_key = "72165fde612945a0a5d08237f3d421e253d77e84ac5bb6172593078401350fc3"
 
 #   # Enable monitoring features
 #   config.enable_performance_monitoring = true
@@ -8,9 +8,9 @@ ActiveRabbit::Client.configure do |config|
 
 #   # Optional: enable deployment tracking
 #   config.revision = ENV["APP_REVISION"] || `git rev-parse HEAD`.chomp
-   config.api_url = "https://app.activerabbit.ai"
-#   config.disable_console_logs = false
+   # config.api_url = "https://app.activerabbit.ai"
+   # config.disable_console_logs = false
 
 #   # Optional: 404 handling (true = ignore 404s, false = report them)
 #   config.ignore_404 = true
-end
+# end
