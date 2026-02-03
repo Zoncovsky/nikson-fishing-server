1293b9aa0745a871f43fe4f5b76ff9002146277b
vmorgunov <vmorgunov10@gmail.com>
2026-02-03 11:10:43 -0800

    error mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :omniauthable, omniauth_providers: %i[google_oauth2]
 
+  has_many :orders, dependent: :destroy
 
   after_create :password_complexity
 
