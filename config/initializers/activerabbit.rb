1050f5f661dc05b1849f3590db570cd33184d0e3
vmorgunov <vmorgunov10@gmail.com>
2026-03-06 04:32:46 -0800

    update ActiveRabbit configuration with new API key and URL

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -1,8 +1,8 @@
-if Rails.env.development?
-  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-end
+# if Rails.env.development?
+#   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+# end
 ActiveRabbit::Client.configure do |config|
-   config.api_key = '04748cd98e7ae7ec7df6aea5dd2372fc18c9c35decb09058dfed71c52793c649'
+   config.api_key = '5c72afd58d4e4222a210305378590f1e122732a524366c948215df53e8161510'
 
   config.enable_performance_monitoring = true
   config.enable_n_plus_one_detection   = true
@@ -11,7 +11,7 @@ ActiveRabbit::Client.configure do |config|
   config.revision = ENV['APP_REVISION'] || `git rev-parse HEAD`.chomp
   config.disable_console_logs = false
   # config.api_url = "https://app.activerabbit.ai"
-  config.api_url = 'https://160ec4227c78.ngrok-free.app'
+  config.api_url = 'https://9ba8-176-120-110-68.ngrok-free.app'
 
   config.ignore_404 = true
 end
