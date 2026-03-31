3398209880280b6575981d11efab1da54266fd90
vmorgunov <vmorgunov10@gmail.com>
2026-03-31 14:29:40 -0700

    fix: correct syntax error in new_arrivals scope and update catalog link in dashboard

--- config/initializers/activerabbit.rb
+++ config/initializers/activerabbit.rb
@@ -2,7 +2,7 @@
 #   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 # end
 ActiveRabbit::Client.configure do |config|
-   config.api_key = '0c12f1c352960b41db9981831f06629b08e8bd95aca4f65fefba238e808de4ae'
+   config.api_key = '640b14abc5c1f2e5f419d939b60e9bd6c265d8feaf78ff1f3a5c7608c6bf09a1'
 
   config.enable_performance_monitoring = true
   config.enable_n_plus_one_detection   = true
@@ -11,7 +11,7 @@ ActiveRabbit::Client.configure do |config|
   config.revision = ENV['APP_REVISION'] || `git rev-parse HEAD`.chomp
   config.disable_console_logs = false
   # config.api_url = 'http://localhost:3003'
-  config.api_url = 'https://26f5-176-120-110-68.ngrok-free.app'
+  config.api_url = 'https://84eb-176-120-110-210.ngrok-free.app'
 
   config.ignore_404 = true
 end
